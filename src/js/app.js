d756da8e8f814494df86442fcacc6d5112953c73
kalaganov5 <kalaganov5@gmail.com>
2022-12-11 19:58:03 +0300

    reviews block

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,41 @@
 import '../scss/app.scss';
 
 /* Your JS Code goes here */
+import Swiper, { Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import { navigationMobile } from '../js/navigation-mobil';
 navigationMobile('body');
+
+var swiper = new Swiper(".reviews__slider", {
+    modules: [Pagination],
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    slidesPerView: 1,
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 640px
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 35
+        }
+    },
+    autoHeight: true
+});
